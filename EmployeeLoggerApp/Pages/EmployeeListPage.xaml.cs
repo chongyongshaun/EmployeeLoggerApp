f34afa2d4cf0dd6b342e3232fc94cbf85820771e
chongyongshaun <chongyongshaun@gmail.com>
2024-10-26 21:42:40 +0100

    Updated detail page function, now able to click on items on listview

--- EmployeeLoggerApp/Pages/EmployeeListPage.xaml.cs
+++ EmployeeLoggerApp/Pages/EmployeeListPage.xaml.cs
@@ -1,3 +1,4 @@
+using EmployeeLoggerApp.Models;
 using EmployeeLoggerApp.Models.ViewModels;
 
 namespace EmployeeLoggerApp.Pages;
@@ -11,8 +12,14 @@ public partial class EmployeeListPage : ContentPage
 		BindingContext = new EmployeesViewModel();
 	}
 
-    private void TextCell_Tapped(object sender, EventArgs e)
+    private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
     {
+		var emp = (Employee) e.Item;
+		//create a new instance of viewmodel obj with the acquired emp data from e.Item
+		var employeeDetailViewModel2 = new EmployeeDetailViewModel2 { Emp = emp};
+		var employeeDetailPage = new EmployeeDetailPage();
+		employeeDetailPage.BindingContext = employeeDetailViewModel2;
 
+		Navigation.PushAsync(employeeDetailPage);
     }
 }
